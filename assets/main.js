becdf30aac1cbba2da370cb7a5422f71cdb967c1
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-02-01 14:05:24 +0000

    Basic JS code to fetching the items from API

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const apiURL = 'https://my-json-server.typicode.com/rhetesi/fnditm';
+
+let itemsArray = [];
+
+// tárgyak lekérése API-ról
+const getItemsFetch = () => {
+    fetch(apiURL)                                                   // meghívjuk az API users lábát, lekérve a user adatokat json formátumban
+    .then(response => response.json())
+    .then(data => {
+        itemsArray = data;                                          // a kapott json-t átadjuk a 9. sorban létrehozott üres tömbnek (későbbiekben ezzel dolgozunk)
+    });
+}
+
+getItemsFetch();
+
+console.log(itemsArray);
