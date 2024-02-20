f6cba880315fdb8d1db6c397da53cef9fe5246e5
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-02-20 14:56:54 +0000

    Basicly works the card grid

--- assets/createCard.js
+++ assets/createCard.js
@@ -6,7 +6,7 @@ export const createCard = (item) => {
         <div class="card" style="width: 18rem;">
           <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">${item.id}e</h5>
+            <h5 class="card-title">${item.name}</h5>
             <p class="card-text">Dátum: ${item.founddate}</p>
             <p class="card-text">${item.description}</p>
             <a href="#" class="btn btn-primary">Megtekint</a>
